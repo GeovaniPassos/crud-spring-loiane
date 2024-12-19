31e45cb19af6486d6c86eb976b0ff4dbb77fcf5d
GeovaniPassos <geovanipassos95@outlook.com>
2024-12-19 10:21:44 -0300

    Implementando o fluxo de salvar curso

--- src/main/java/com/loiane_cursos/crud_spring_api/model/Course.java
+++ src/main/java/com/loiane_cursos/crud_spring_api/model/Course.java
@@ -1,15 +1,16 @@
 package com.loiane_cursos.crud_spring_api.model;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import jakarta.persistence.*;
 import lombok.Data;
 
 @Data
-@Entity
-//@Table(name = "curso")
+@Entity  //@Table(name = "curso")
 public class Course {
 
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
+    @JsonProperty("_id")
     private Long id;
 
     @Column(length = 200, nullable = false)
@@ -18,4 +19,27 @@ public class Course {
     @Column(length = 20, nullable = false)
     private String category;
 
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
 }
